bf5429d1cb2b69ad0e8613b737affd724c0b9f10
Tiffany <tiffanyth.1812@gmail.com>
2025-03-19 00:22:27 +0100

    page index faite

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,41 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, city, country, tagline, price, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", `Portrait de ${name}`); 
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const locationElement = document.createElement('p');
+        locationElement.innerText = `${city}, ${country}`;
+        locationElement.classList.add("location"); 
+        locationElement.setAttribute("aria-label", `Localisation : ${city}, ${country}`);
+
+
+        const taglineElement = document.createElement('p');
+        taglineElement.innerText = tagline;
+        taglineElement.classList.add("tagline"); 
+        taglineElement.setAttribute("aria-label", `Slogan : ${tagline}`); 
+
+
+        const priceElement = document.createElement('p');
+        priceElement.innerText = `${price}€/jour`;
+        priceElement.classList.add("price"); 
+        priceElement.setAttribute("aria-label", `Tarif journalier : ${price}`); 
+
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(locationElement);
+        article.appendChild(taglineElement);
+        article.appendChild(priceElement);
         return (article);
     }
     return { name, picture, getUserCardDOM }
